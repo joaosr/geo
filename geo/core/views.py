43c9be21ad5549091d341aa7a0da980fa2f9a49f
joaosr <eng.jmsoares@gmail.com>
2016-11-04 21:41:34 -0300

    Set indicator true in method merge

--- geo/core/views.py
+++ geo/core/views.py
@@ -7,11 +7,14 @@ from django.shortcuts import render
 # Create your views here.
 FILE_ROOT = os.path.join(BASE_DIR, 'files')
 
+
 def municipios(request):
     return JsonResponse({"municipios":list_municipios()})
 
+
 def categorias(request):
     return JsonResponse({"categorias":list_categorias()})
+
 
 def list_municipios():
     pmv_csv = pd.read_csv(os.path.join(FILE_ROOT, 'pmv.csv'))
@@ -19,11 +22,13 @@ def list_municipios():
     _municipios = df['municipio'].unique()
     return _municipios.tolist()
 
+
 def list_categorias():
     pmv_csv = pd.read_csv(os.path.join(FILE_ROOT, 'pmv.csv'))
     df = pd.DataFrame(pmv_csv)
     _subcategorias = df['subcategoria'].unique()
     return _subcategorias.tolist()
+
 
 def filter_municipio_categoria(municipio_id, categoria):
     pmv_csv = pd.read_csv(os.path.join(FILE_ROOT, 'pmv.csv'))
@@ -33,15 +38,13 @@ def filter_municipio_categoria(municipio_id, categoria):
     data = df[_municipios & _subcategorias]
     return data.set_index('categoria').T.to_dict()
 
+
 def is_new_table(old, new):
-    csv_old = pd.read_csv(os.path.join(FILE_ROOT, old))
-    csv_new = pd.read_csv(os.path.join(FILE_ROOT, new))
-    dfo = pd.DataFrame(csv_old)
-    dfn = pd.DataFrame(csv_new)
-    common_cols = list(set(dfo.columns) & set(dfn.columns))
-    #result = pd.merge(dfo, dfn, on=common_cols, how='inner')
-    result = dfo.join(dfn, on=common_cols, how='inner')
-    orows = len(dfo.index)
-    rows = len(result.index)
-    print(orows, rows)
-    return False if rows > 0 else True
+    dfo = pd.read_csv(os.path.join(FILE_ROOT, old))
+    dfn = pd.read_csv(os.path.join(FILE_ROOT, new))
+    common_cols = list(dfo.columns.values)
+    print(common_cols)
+    df = pd.merge(dfo, dfn, on=common_cols, how='outer', indicator=True)
+    result = df[df['_merge'] == 'both']
+    print(result)
+    return False if len(result.index) > 0 else True
